05b9399dbef40ced5a8b9e5356a19c63599d7dad
lianghuahuang <huahuangliang@163.com>
2021-08-26 17:20:56 +0800

    修复闭包下的序列化报错问题

--- spark-core-work/src/main/scala/SparkDistCP.scala
+++ spark-core-work/src/main/scala/SparkDistCP.scala
@@ -1,4 +1,3 @@
-import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.{FileSystem, FileUtil, Path}
 import org.apache.spark.sql.SparkSession
 
@@ -13,82 +12,82 @@ import scala.collection.mutable.ListBuffer
  *
  * */
 object SparkDistCP {
+  System.setProperty("HADOOP_USER_NAME", "root")
+  val spark = SparkSession.builder()
+    .master("local[*]").appName("SparkDistCp")
+    .config("spark.driver.host", "192.168.0.105")
+    .config("spark.driver.bindAddress", "192.168.0.105")
+    .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+    .enableHiveSupport()
+    .getOrCreate()
+  val sc = spark.sparkContext;
+
   def main(args: Array[String]): Unit = {
-    System.setProperty("HADOOP_USER_NAME","root")
-    val spark = SparkSession.builder()
-      .master("local[*]").appName("SparkDistCp")
-      .config("spark.driver.host","192.168.0.105")
-      .config("spark.driver.bindAddress","192.168.0.105")
-      .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
-      .enableHiveSupport()
-      .getOrCreate()
-    @transient   val sc = spark.sparkContext;
-    @transient   val srcPath: Path =new Path("hdfs://master:9000/aa");
-    @transient   val destPath: Path = new Path("hdfs://master:9000/cc")
-    val maxCurrence =2
-    sc.hadoopConfiguration.setBoolean("mapreduce.input.fileinputformat.input.dir.recursive",true)
-    @transient val sfileSystem:FileSystem = srcPath.getFileSystem(sc.hadoopConfiguration)
-    @transient val dfileSystem:FileSystem = destPath.getFileSystem(sc.hadoopConfiguration)
-    checkDirectories(srcPath,destPath,sfileSystem,dfileSystem);
-    val fileIter = srcPath.getFileSystem(sc.hadoopConfiguration).listFiles(srcPath,true);
-    val fileList = new mutable.MutableList[(String,String)]
-    while(fileIter.hasNext){
+    val srcPath: Path = new Path("hdfs://master:9000/aa");
+    val destPath: Path = new Path("hdfs://master:9000/cc")
+    val sfileSystem: FileSystem = srcPath.getFileSystem(sc.hadoopConfiguration)
+    val dfileSystem: FileSystem = destPath.getFileSystem(sc.hadoopConfiguration)
+    checkDirectories(srcPath, destPath, sfileSystem, dfileSystem);
+    val fileIter = srcPath.getFileSystem(sc.hadoopConfiguration).listFiles(srcPath, true);
+    val fileList = new mutable.MutableList[(String, String)]
+    while (fileIter.hasNext) {
       val f = fileIter.next();
-      val subPath  = f.getPath.toString.split(srcPath.toString())(1)
-      val s =  destPath.toString + subPath
-      fileList.+=:(f.getPath.toString,s)
+      val subPath = f.getPath.toString.split(srcPath.toString())(1)
+      val s = destPath.toString + subPath
+      fileList.+=:(f.getPath.toString, s)
     }
-
-    val fileRDD = sc.makeRDD(fileList,maxCurrence)
-
-    fileRDD.mapPartitions(v=>transfrom(v, sfileSystem, dfileSystem,sc.hadoopConfiguration)).collect().foreach(println)
+    val maxCurrence = 2
+    val fileRDD = sc.makeRDD(fileList, maxCurrence)
+    fileRDD.mapPartitions(v => transfrom(v)).collect().foreach(println)
   }
 
-  def transfrom(v: Iterator[(String,String)],sfileSystem:FileSystem,dfileSystem:FileSystem,hadoopConfiguration: Configuration): Iterator[(String,Boolean)] = {
-    val list = new mutable.MutableList[(String,Boolean)]
-    while(v.hasNext){
+  def transfrom(v: Iterator[(String, String)]): Iterator[(String, Boolean)] = {
+    val list = new mutable.MutableList[(String, Boolean)]
+    val sc1 = this.sc;
+    while (v.hasNext) {
       val p = v.next()
-      val result = FileUtil.copy(sfileSystem,new Path(p._1),dfileSystem,new Path(p._2),false,hadoopConfiguration)
-      list.+=:(p._2,result)
+      val sfs: FileSystem = new Path(p._1).getFileSystem(sc1.hadoopConfiguration)
+      val dfs: FileSystem = new Path(p._2).getFileSystem(sc1.hadoopConfiguration)
+      val result = FileUtil.copy(sfs, new Path(p._1), dfs, new Path(p._2), false, sc1.hadoopConfiguration)
+      list.+=:(p._2, result)
     }
     list.iterator
   }
 
-/*  def run(sparkSession: SparkSession,srcPath:Path,distPath:Path,options:SparkDistCPOptions): Unit ={
-    options.validateOptions()
-    sparkSession.sparkContext.hadoopConfiguration.setBoolean("mapreduce.input.fileinputformat.input.dir.recursive",true)
-    val qualifiedSrcPaths = pathToQualifiedPath(sparkSession.sparkContext.hadoopConfiguration, srcPath)
-    val qualifiedDestPath = pathToQualifiedPath(sparkSession.sparkContext.hadoopConfiguration, distPath)
-  }
+  /*  def run(sparkSession: SparkSession,srcPath:Path,distPath:Path,options:SparkDistCPOptions): Unit ={
+      options.validateOptions()
+      sparkSession.sparkContext.hadoopConfiguration.setBoolean("mapreduce.input.fileinputformat.input.dir.recursive",true)
+      val qualifiedSrcPaths = pathToQualifiedPath(sparkSession.sparkContext.hadoopConfiguration, srcPath)
+      val qualifiedDestPath = pathToQualifiedPath(sparkSession.sparkContext.hadoopConfiguration, distPath)
+    }
 
-  def pathToQualifiedPath(hadoopConfiguration: Configuration, path: Path): Path = {
-    val fs = FileSystem.get(hadoopConfiguration)
-    path.makeQualified(fs.getUri, fs.getWorkingDirectory)
-  }*/
+    def pathToQualifiedPath(hadoopConfiguration: Configuration, path: Path): Path = {
+      val fs = FileSystem.get(hadoopConfiguration)
+      path.makeQualified(fs.getUri, fs.getWorkingDirectory)
+    }*/
 
 
   /**
    * 创建目标目录文件夹
+   *
    * @param srcPath
    * @param destPath
    * @return
    */
-  def checkDirectories(srcPath:Path,destPath:Path,sfileSystem:FileSystem,dfileSystem:FileSystem): ListBuffer[String] ={
+  def checkDirectories(srcPath: Path, destPath: Path, sfileSystem: FileSystem, dfileSystem: FileSystem): ListBuffer[String] = {
     //val list = new mutable.MutableList[(String,String)]
-    val fileList  =new ListBuffer[String]()
+    val fileList = new ListBuffer[String]()
     sfileSystem.listStatus(srcPath).foreach(f => {
-      if(f.isDirectory()){
-        val subPath  = f.getPath.toString.split(srcPath.toString())(1)
-        val s =  destPath.toString + subPath
+      if (f.isDirectory()) {
+        val subPath = f.getPath.toString.split(srcPath.toString())(1)
+        val s = destPath.toString + subPath
         dfileSystem.mkdirs(new Path(s))
         val p = new Path(destPath + subPath)
-        checkDirectories(f.getPath,p,sfileSystem, dfileSystem)
-      }else{
-      //  list.+=: (f.getPath.toString,destPath.toString)
+        checkDirectories(f.getPath, p, sfileSystem, dfileSystem)
+      } else {
         fileList.append(f.getPath.toString)
       }
     })
-    println(fileList)
     fileList
   }
 }
