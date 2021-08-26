85f80dd249f7f162fa360e61fab85721c233558a
lianghuahuang <huahuangliang@163.com>
2021-08-26 15:45:41 +0800

    Update SparkDistCP.scala

--- spark-core-work/src/main/scala/SparkDistCP.scala
+++ spark-core-work/src/main/scala/SparkDistCP.scala
@@ -3,6 +3,7 @@ import org.apache.hadoop.fs.{FileSystem, FileUtil, Path}
 import org.apache.spark.sql.SparkSession
 
 import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
 
 /**
  * ${DESCRIPTION}
@@ -13,34 +14,47 @@ import scala.collection.mutable
  * */
 object SparkDistCP {
   def main(args: Array[String]): Unit = {
+    System.setProperty("HADOOP_USER_NAME","root")
     val spark = SparkSession.builder()
       .master("local[*]").appName("SparkDistCp")
       .config("spark.driver.host","192.168.0.105")
       .config("spark.driver.bindAddress","192.168.0.105")
+      .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
       .enableHiveSupport()
       .getOrCreate()
-    val sc = spark.sparkContext;
-    val srcPath: Path =new Path("hdfs://master:9000/aa/bb");
-    val destPath: Path = new Path("hdfs://master:9000/cc")
+    @transient   val sc = spark.sparkContext;
+    @transient   val srcPath: Path =new Path("hdfs://master:9000/aa");
+    @transient   val destPath: Path = new Path("hdfs://master:9000/cc")
     val maxCurrence =2
     sc.hadoopConfiguration.setBoolean("mapreduce.input.fileinputformat.input.dir.recursive",true)
-    val fileSystem:FileSystem = srcPath.getFileSystem(sc.hadoopConfiguration)
-    val fileList  = checkDirectories(srcPath,destPath,fileSystem);
-
+    @transient val sfileSystem:FileSystem = srcPath.getFileSystem(sc.hadoopConfiguration)
+    @transient val dfileSystem:FileSystem = destPath.getFileSystem(sc.hadoopConfiguration)
+    checkDirectories(srcPath,destPath,sfileSystem,dfileSystem);
+    val fileIter = srcPath.getFileSystem(sc.hadoopConfiguration).listFiles(srcPath,true);
+    val fileList = new mutable.MutableList[(String,String)]
+    while(fileIter.hasNext){
+      val f = fileIter.next();
+      val subPath  = f.getPath.toString.split(srcPath.toString())(1)
+      val s =  destPath.toString + subPath
+      fileList.+=:(f.getPath.toString,s)
+    }
 
     val fileRDD = sc.makeRDD(fileList,maxCurrence)
-    fileRDD.mapPartitions(v=>{
-      val list = new mutable.MutableList[(String,Boolean)]
-      while(v.hasNext){
-        val p = v.next()
-        val result = FileUtil.copy(fileSystem,new Path(p._1),fileSystem,new Path(p._2),false,sc.hadoopConfiguration)
-        list.+=:(p._1,result)
-      }
-      return list
-    }).collect().foreach(println)
+
+    fileRDD.mapPartitions(v=>transfrom(v, sfileSystem, dfileSystem,sc.hadoopConfiguration)).collect().foreach(println)
   }
 
-  def run(sparkSession: SparkSession,srcPath:Path,distPath:Path,options:SparkDistCPOptions): Unit ={
+  def transfrom(v: Iterator[(String,String)],sfileSystem:FileSystem,dfileSystem:FileSystem,hadoopConfiguration: Configuration): Iterator[(String,Boolean)] = {
+    val list = new mutable.MutableList[(String,Boolean)]
+    while(v.hasNext){
+      val p = v.next()
+      val result = FileUtil.copy(sfileSystem,new Path(p._1),dfileSystem,new Path(p._2),false,hadoopConfiguration)
+      list.+=:(p._2,result)
+    }
+    list.iterator
+  }
+
+/*  def run(sparkSession: SparkSession,srcPath:Path,distPath:Path,options:SparkDistCPOptions): Unit ={
     options.validateOptions()
     sparkSession.sparkContext.hadoopConfiguration.setBoolean("mapreduce.input.fileinputformat.input.dir.recursive",true)
     val qualifiedSrcPaths = pathToQualifiedPath(sparkSession.sparkContext.hadoopConfiguration, srcPath)
@@ -50,7 +64,7 @@ object SparkDistCP {
   def pathToQualifiedPath(hadoopConfiguration: Configuration, path: Path): Path = {
     val fs = FileSystem.get(hadoopConfiguration)
     path.makeQualified(fs.getUri, fs.getWorkingDirectory)
-  }
+  }*/
 
 
   /**
@@ -59,19 +73,22 @@ object SparkDistCP {
    * @param destPath
    * @return
    */
-  def checkDirectories(srcPath:Path,destPath:Path,fileSystem:FileSystem): mutable.MutableList[(String,String)] ={
-    val list = new mutable.MutableList[(String,String)]
-    fileSystem.listStatus(destPath).foreach(f => {
+  def checkDirectories(srcPath:Path,destPath:Path,sfileSystem:FileSystem,dfileSystem:FileSystem): ListBuffer[String] ={
+    //val list = new mutable.MutableList[(String,String)]
+    val fileList  =new ListBuffer[String]()
+    sfileSystem.listStatus(srcPath).foreach(f => {
       if(f.isDirectory()){
         val subPath  = f.getPath.toString.split(srcPath.toString())(1)
-        val s = srcPath.toUri + destPath.toString + subPath
-        fileSystem.mkdirs(new Path(s))
+        val s =  destPath.toString + subPath
+        dfileSystem.mkdirs(new Path(s))
         val p = new Path(destPath + subPath)
-        checkDirectories(f.getPath,p,fileSystem)
+        checkDirectories(f.getPath,p,sfileSystem, dfileSystem)
       }else{
-        list.+=: (f.getPath.toString,destPath.toString)
+      //  list.+=: (f.getPath.toString,destPath.toString)
+        fileList.append(f.getPath.toString)
       }
     })
-    list
+    println(fileList)
+    fileList
   }
 }
